563fbcdb601760a2aa9da7fe4e6066804efcfe4f
Ismail Bay <ismailbay@gmail.com>
2023-02-18 21:33:48 +0100

    improve terraform plan, output ips for inventory

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,87 +1,111 @@
+resource "proxmox_vm_qemu" "proxmox_vm_master" {
+  count = var.num_k3s_masters
+  name  = "k3s-master-0${count.index + 1}"
+  desc  = "K3S Master Node"
+
+  sockets = "1"
+  cores   = 4
+  memory  = 2048
+  scsihw  = "virtio-scsi-pci"
+
+  ipconfig0   = "gw=192.168.1.1,ip=${var.k3s_master_ip_addresses[count.index]}"
+  target_node = "pve"
+  onboot      = true
+  oncreate    = true
+  full_clone  = true
+  clone       = var.k8s_source_template
+  os_type     = "cloud-init"
+  agent       = 1
+  tablet      = false
+  ciuser      = var.k3s_user
+  sshkeys     = var.vm_ssh_keys
+
+  disk {
+    size    = "20G"
+    type    = "scsi"
+    storage = "ssd-pool"
+    ssd     = 1
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  serial {
+    id   = 0
+    type = "socket"
+  }
+
+  vga {
+    type = "serial0"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network, disk, sshkeys, target_node
+    ]
+  }
+}
+
+resource "proxmox_vm_qemu" "proxmox_vm_worker" {
+  count = var.num_k3s_workers
+  name  = "k3s-worker-0${count.index + 1}"
+  desc  = "K3S Worker Node"
+
+  sockets = "1"
+  cores   = 4
+  memory  = 8192
+  scsihw  = "virtio-scsi-pci"
+
+  ipconfig0   = "gw=192.168.1.1,ip=${var.k3s_worker_ip_addresses[count.index]}"
+  target_node = "pve"
+  onboot      = true
+  oncreate    = true
+  full_clone  = true
+  clone       = var.k8s_source_template
+  os_type     = "cloud-init"
+  agent       = 1
+  tablet      = false
+  ciuser      = var.k3s_user
+  sshkeys     = var.vm_ssh_keys
+
+  disk {
+    size    = "30G"
+    type    = "scsi"
+    storage = "ssd-pool"
+    ssd     = 1
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  serial {
+    id   = 0
+    type = "socket"
+  }
+
+  vga {
+    type = "serial0"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network, disk, sshkeys, target_node
+    ]
+  }
+}
+
 locals {
-  k8s_source_template = "ubuntu-cloud"
-
-  k8s_masters = {
-    m01 = { vmid = "101", name = "master01.k8s.ismailbay.at", ip = "192.168.1.101" },
-    m02 = { vmid = "102", name = "master02.k8s.ismailbay.at", ip = "192.168.1.102" },
-    m03 = { vmid = "103", name = "master03.k8s.ismailbay.at", ip = "192.168.1.103" }
-  }
-
-  k8s_workers = {
-    w01 = { vmid = "104", name = "worker01.k8s.ismailbay.at", ip = "192.168.1.104" },
-    w02 = { vmid = "105", name = "worker02.k8s.ismailbay.at", ip = "192.168.1.105" },
-    w03 = { vmid = "106", name = "worker03.k8s.ismailbay.at", ip = "192.168.1.106" }
-  }
-
+  k3s_rendered = templatefile("./templates/k3s.tftpl", {
+    master_ips = join("\n", proxmox_vm_qemu.proxmox_vm_master.*.default_ipv4_address)
+    worker_ips = join("\n", proxmox_vm_qemu.proxmox_vm_worker.*.default_ipv4_address)
+  })
 }
 
-resource "proxmox_vm_qemu" "ubuntu-k8s-masters" {
-  for_each    = local.k8s_masters
-  vmid        = each.value.vmid
-  name        = each.value.name
-  desc        = each.value.name
-  target_node = "pve"
-  os_type     = "cloud-init"
-  full_clone  = true
-  memory      = 8192
-  sockets     = "1"
-  cores       = 4
-  cpu         = "host"
-  scsihw      = "virtio-scsi-pci"
-  clone       = local.k8s_source_template
-  ciuser      = "ismail"
-  sshkeys     = var.vm_ssh_keys
-  tags        = "k8s"
-  disk {
-    size    = "30G"
-    type    = "scsi"
-    storage = "ssd-pool"
-  }
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  # Cloud-init section
-  ipconfig0 = "ip=${each.value.ip}/24,gw=192.168.1.1"
-
-  provisioner "local-exec" {
-    # command = "ansible-playbook -i '${each.value.ip}', playbook/*"
-    command = "echo '${each.value.ip}'"
-  }
-}
-
-resource "proxmox_vm_qemu" "ubuntu-k8s-workers" {
-  for_each    = local.k8s_workers
-  vmid        = each.value.vmid
-  name        = each.value.name
-  desc        = each.value.name
-  target_node = "pve"
-  os_type     = "cloud-init"
-  full_clone  = true
-  memory      = 8192
-  sockets     = "1"
-  cores       = 4
-  cpu         = "host"
-  scsihw      = "virtio-scsi-pci"
-  clone       = local.k8s_source_template
-  ciuser      = "ismail"
-  sshkeys     = var.vm_ssh_keys
-  tags        = "k8s"
-  disk {
-    size    = "30G"
-    type    = "scsi"
-    storage = "ssd-pool"
-  }
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  # Cloud-init section
-  ipconfig0 = "ip=${each.value.ip}/24,gw=192.168.1.1"
-
-  provisioner "local-exec" {
-    command = "echo '${each.value.ip}'"
-  }
+resource "local_file" "k3s_inventory" {
+  content = local.k3s_rendered
+  filename = "./ansible/inventory.ini"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,31 @@ variable "vm_ssh_keys" {
     description = "ssh keys used in VMs"
     type = string
 }
+
+variable "num_k3s_masters" {
+  default = 3
+}
+
+variable "k3s_master_ip_addresses" {
+  description = "List of IP addresses for master node(s)"
+  type        = list(string)
+  default     = ["192.168.1.101/24", "192.168.1.102/24", "192.168.1.103/24"]
+}
+
+variable "num_k3s_workers" {
+  default = 3
+}
+
+variable "k3s_worker_ip_addresses" {
+  description = "List of IP addresses for master node(s)"
+  type        = list(string)
+  default     = ["192.168.1.104/24", "192.168.1.105/24", "192.168.1.106/24"]
+}
+
+variable "k8s_source_template" {
+  default = "ubuntu-cloud"
+}
+
+variable "k3s_user" {
+  default = "ismail"
+}
